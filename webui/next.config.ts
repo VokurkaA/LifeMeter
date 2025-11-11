0ddb6b8d1b94f7fc7834478c095ec2776cf035c9
Vokurkaa <zadrobilek.jan@seznam.cz>
2025-11-11 12:21:52 +0100

    feat: Update .gitignore and package.json for webui and server configurations

--- webui/next.config.ts
+++ webui/next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://server-production-282f.up.railway.app/:path*'
+      }
+    ];
+  },
+
 };
 
 export default nextConfig;
