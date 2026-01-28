09bfadf0be99bf55fd35c9b5df76855fa4121045
Vokurkaa <zadrobilek.jan@seznam.cz>
2026-01-28 02:48:49 +0100

    .

--- App/src/screens/app/nutrition/Index.screen.tsx
+++ App/src/screens/app/nutrition/Index.screen.tsx
@@ -1,16 +1,10 @@
-import { View } from 'react-native';
-import CameraScreen from "./components/CameraScreen";
 import MainLayout from '@/layouts/Main.layout';
 import AddNewMeal from './components/AddNewMeal.sheet';
-import { ScrollableWithSnapPointsContent } from './components/scrollable-with-snap-points';
 
 export default function NutritionScreen() {
     return (
         <MainLayout>
             <AddNewMeal/>
-            <ScrollableWithSnapPointsContent/>
-             {/* <CameraScreen/> */}
-
         </MainLayout>
     );
 }
